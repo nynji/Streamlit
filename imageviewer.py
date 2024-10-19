361aaf822c87c93dc737519ef3f207f853ab458b
HJ <12210544a@inha.edu>
2024-10-19 18:40:29 +0900

    imageviewer

--- imageviewer.py
+++ imageviewer.py
@@ -2,43 +2,54 @@ import streamlit as st
 import cv2
 import numpy as np
 
-# 제목
-st.title("이미지 변환 및 회전")
 
-# 이미지 파일 업로드
+st.title("ImageViewer System")
+
+
 uploaded_file = st.file_uploader("이미지 파일을 업로드하세요", type=["png", "jpg", "jpeg"])
 
 if uploaded_file is not None:
-    # 이미지 열기
+
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # 회전 각도 초기화
     if 'angle' not in st.session_state:
         st.session_state.angle = 0
 
-    # 슬라이더로 각도 조정
     st.session_state.angle = st.slider("회전 각도", 0, 360, st.session_state.angle)
+    brightness = st.slider("밝기 조정", -100, 100, 0)
 
-    # 두 개의 열 생성
-    col1, col2 = st.columns(2)
+    with st.container():
+        st.header("이미지 변환")
+        col1, col2, col3 = st.columns(3)
 
-    # 왼쪽 열에 원본 이미지 표시
-    with col1:
-        st.image(image, caption='원본 이미지', use_column_width=True)
+        with col1:
+            st.image(image, caption='원본 이미지', use_column_width=True)
 
-    # 오른쪽 열에 변환된 이미지 표시
-    with col2:
-        # 회전된 이미지 계산
-        h, w = image.shape[:2]
-        center = (w // 2, h // 2)
-        rotation_matrix = cv2.getRotationMatrix2D(center, st.session_state.angle, 1.0)
-        rotated_image = cv2.warpAffine(image, rotation_matrix, (w, h))
+        with col2:
+            h, w = image.shape[:2]
+            center = (w // 2, h // 2)
+            rotation_matrix = cv2.getRotationMatrix2D(center, st.session_state.angle, 1.0)
+            rotated_image = cv2.warpAffine(image, rotation_matrix, (w, h))
+            adjusted_image = cv2.convertScaleAbs(rotated_image, alpha=1, beta=brightness)
+            st.image(adjusted_image, caption=f'조정된 이미지: {st.session_state.angle}도, {brightness} 밝기', use_column_width=True)
 
-        st.image(rotated_image, caption=f'회전된 이미지: {st.session_state.angle}도', use_column_width=True)
-
-        if st.button('그레이스케일 변환'):
-            gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-            with col2:
+        with col3:
+            if st.button('gray'):
+                gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
                 st.image(gray_image, caption='그레이스케일 이미지', use_column_width=True)
+
+    with st.container():
+        st.header("이미지 크롭")
+        x1 = st.number_input("크롭 시작 X 좌표", min_value=0, max_value=image.shape[1]-1, value=0)
+        y1 = st.number_input("크롭 시작 Y 좌표", min_value=0, max_value=image.shape[0]-1, value=0)
+        x2 = st.number_input("크롭 끝 X 좌표", min_value=0, max_value=image.shape[1]-1, value=image.shape[1]-1)
+        y2 = st.number_input("크롭 끝 Y 좌표", min_value=0, max_value=image.shape[0]-1, value=image.shape[0]-1)
+
+        if st.button("이미지 크롭"):
+            cropped_image = image[y1:y2, x1:x2]
+            st.image(cropped_image, caption='크롭된 이미지', use_column_width=True)
+        
+
+
